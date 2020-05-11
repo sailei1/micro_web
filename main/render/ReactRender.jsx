2829125bea57f785828138bcf9fb1b8df139d415
张赛磊 <saileizhang7@gmail.com>
2020-05-11 16:57:15 +0800

    init

---
+++ main/render/ReactRender.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * 渲染子应用
+ */
+function Render(props) {
+  const { appContent, loading } = props;
+
+  return (
+    <>
+      {loading && (
+        <h4 className="subapp-loading">Loading...</h4>
+      )}
+      <div id="subapp-viewport"/>
+      <div dangerouslySetInnerHTML={{ __html: appContent }}/>
+    </>
+  );
+}
+
+export default function render({ appContent, loading }) {
+  const container = document.getElementById('subapp-container');
+  ReactDOM.render(
+    <Render appContent={appContent} loading={loading}/>,
+    container,
+  );
+}
